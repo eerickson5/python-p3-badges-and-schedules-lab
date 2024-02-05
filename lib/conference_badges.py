8e9ce889b6a8c2e57f264f5bfe82c50303a13b2a
Elise Erickson <elisece717@gmail.com>
2024-02-05 07:54:10 -0500

    done

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,17 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
+
+def room_assigner(name, room):
+    return f"Hello, {name}! You'll be assigned to room {room}!"
 
 def assign_rooms(names):
-    return None
+    return [room_assigner(names[i], i + 1) for i in range(len(names))]
 
 def printer(names):
-    return None
+    for mes in batch_badge_creator(names):
+        print(mes)
+    for mes in assign_rooms(names):
+        print(mes)
